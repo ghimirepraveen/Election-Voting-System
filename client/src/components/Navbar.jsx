bd65778acd43aed24d835017ffe56d00dba92547
ghimirepraveen <ghimireprabin7777@gmail.com>
2024-04-26 15:54:59 +0545

    adding navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-400 w-full h-16 flex items-center justify-center">
+      <h1 className="font-semibold text-2xl">ELECTION</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
